f52180c0a672f7528c69b4e3e6296afee688e209
west-field <fko2247034@stu.o-hara.ac.jp>
2022-09-28 11:35:09 +0900

    移動開始までの待ち時間を追加

--- car.cpp
+++ car.cpp
@@ -2,10 +2,21 @@
 #include "game.h"
 #include "car.h"
 
+namespace
+{
+	//‘Ò‚¿ŽžŠÔ
+	constexpr int kWaitFrameMin = 60;
+	constexpr int kWaitFrameMax = 180;
+
+	//ŽÔ‚Ì‘¬“x
+	constexpr float kSpeed = -20.0f;
+}
+
 Car::Car()
 {
 	m_handle = -1;
 	m_fieldY = 0.0f;
+	m_waitFrame = 0;
 }
 
 void Car::setGraphic(int handle)
@@ -20,16 +31,26 @@ void Car::setup(float fieldY)
 	m_pos.x = Game::kScreenWidth + 16.0f;
 	m_pos.y = m_fieldY - m_size.y;
 
-	m_vec.x = -16.0f;
+	m_vec.x = kSpeed;
 	m_vec.y = 0.0f;
+
+	//“®‚«Žn‚ß‚é‚Ü‚Å‚ÌŽžŠÔ‚ðÝ’è 1•b‚©‚ç3•b‘Ò‚Â
+	m_waitFrame = GetRand(kWaitFrameMax) + kWaitFrameMin;
 }
 
 void Car::update()
 {
+	if (m_waitFrame > 0)
+	{
+		m_waitFrame--;
+		return;
+	}
+
 	m_pos += m_vec;
 }
 
 void Car::draw()
 {
 	DrawGraphF(m_pos.x, m_pos.y, m_handle, true);
+//	DrawFormatString(0, 0, GetColor(255, 255, 255), "wait:%d", m_waitFrame);
 }
